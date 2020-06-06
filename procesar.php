03a61f5878045e76fe5329de38d4790340be4618
amayrani71 <66509520+amayrani71@users.noreply.github.com>
2020-06-06 02:33:07 -0500

    Add files via upload

---
+++ procesar.php
@@ -0,0 +1,11 @@
+<?php
+
+$n = $_POST["nombre"];
+$e = $_POST["edad"];
+$o = $_POST["opinion"];
+
+echo "<h2>Informacion recibida desde PHP</h2>";
+echo "El nombre recibido es:".$n."<br/>";
+echo "La edad recibida es:".$e."<br/>";
+echo "Recibimos tu siguiente opinion:".$o."<br/>";
+?>
